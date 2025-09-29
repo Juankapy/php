88ced0c8ed4e550a5ead22c1b70dd3b348416db3
Juank <jpereavelasco@safareyes.es>
2025-09-29 10:53:33 +0200

    subir

---
+++ actividades/bucles/EJ1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 Bucles</title>
+</head>
+<body>
+    <?php
+    echo"Bucle de multiplos de 5 hasta el 100";
+    for ($i = 0; $i < 0; $i++) {
+        echo "<br>
+        echo $i * 5;";  
+    }
+    echo "<br>";
+    ?>
+</body>
+</html>
